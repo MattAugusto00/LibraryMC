dd21e5673f03602bf84e1fbe47341fc7759af796
MattAugusto00 <58348794+MattAugusto00@users.noreply.github.com>
2022-09-20 11:23:24 -0300

    Update C_listarLivrosEstudante.php

--- Código/Controller/C_listarLivrosEstudante.php
+++ Código/Controller/C_listarLivrosEstudante.php
@@ -23,19 +23,11 @@ $res = $livroDAO->consultar($conn);
     <link rel="stylesheet" href="../View/css/listarLivrosEstudante.css" />
 </head>
 <body>
-    <!--Cabeçalho-->
     <header>.</header>
-
-    <!--Se�ao nao tava descendo por motivos que nao sei, logo criei para deixar a se�ao no meio da pagina rsrs-->
     <div class="moveSecao">
-        <!--Dentro da section terao 2 Divisoes. A primeira para o formulario do Estudante, a segunda para o formulario do Funcionario-->
-        <!--Coloquei as 2 divisoes dentro da mesma section para facilitar no alinhamento delas com uso do flexbox no CSS-->
         <section class="secao">
-            <!--Primeira divisao(Estudante)-->
             <div class="welcome">
-                <!--Titulo da divisao-->
                 <h1>Página do Estudante</h1>
-                <!--Segunda Divisão(Funcionario)-->
                 <div class="opcoes">
                     <a href="../View/html/meusDadosEstudante.html">
                         <div>Meus Dados</div>
@@ -86,8 +78,6 @@ $res = $livroDAO->consultar($conn);
             </div>
         </section>
     </div>
-
-    <!--Rodap� da pagina-->
     <footer>.</footer>
 </body>
 </html>
