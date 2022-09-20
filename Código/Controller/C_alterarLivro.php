efbb87a5031e2e177dfe16243fd289568af61a60
Carlos Magno do Nascimento Junior <69009883+carlos-junior2@users.noreply.github.com>
2022-09-19 22:57:50 -0300

    Create C_alterarLivro.php

---
+++ Código/Controller/C_alterarLivro.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '..\Persistence\Connection.php';
+include_once '..\Model\Livro.php';
+include_once '..\Persistence\LivroDAO.php';
+
+$id = $_POST['id'];
+$titulo = $_POST['titulo'];
+$autor = $_POST['autor'];
+$editora = $_POST['editora'];
+$ano = $_POST['ano'];
+$genero = $_POST['genero'];
+
+
+$conexao = new Connection();
+$conn = $conexao->getConnection();
+
+$livro = new Livro($titulo, $autor, $editora, $ano, $genero);
+
+$livroDAO = new LivroDAO();
+
+$livroDAO->alterar($conn, $id, $livro);
+
+?>
