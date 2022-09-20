e2eb003024bfb6933f745899551dc50813cb5f6a
Carlos Magno do Nascimento Junior <69009883+carlos-junior2@users.noreply.github.com>
2022-09-20 01:23:20 -0300

    Update C_listarLivrosFuncionario.php

--- Código/Controller/C_listarLivrosFuncionario.php
+++ Código/Controller/C_listarLivrosFuncionario.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 include_once '..\Persistence\Connection.php';
 include_once '..\Persistence\LivroDAO.php';
@@ -71,10 +71,10 @@ $res = $livroDAO->consultar($conn);
                                 "<td>".$linha['ano']."</td>".
                                 "<td>".$linha['genero']."</td>".
                                 "<div class="."botoes".">".
-                                "<td><a href="."alterarLivro.php?cod=$cod".">"."<img border="."0"." alt="."alterar"." src="."../View/icons/editar.svg"." width="."30"." height="."30"."></a>".
+                                "<td><a href="."C_dadosLivro.php?cod=$cod".">"."<img border="."0"." alt="."alterar"." src="."../View/icons/editar.svg"." width="."30"." height="."30"."></a>".
                                 "<td><a href=".""."><img border="."0"." alt="."listaDeEspera"." src="."../View/icons/lista.svg"." width="."30"." height="."30"."></a>".
                                 "<td><a href=".""."><img border="."0"." alt="."alterar"." src="."../View/icons/info.svg"." width="."30"." height="."30"."></a>".
-                                "<td><a href=".""."><img border="."0"." alt="."alterar"." src="."../View/icons/deletar.svg"." width="."30"." height="."30"."></a>".
+                                "<td><a href="."C_confirmarExclusaoLivro.php?cod=$cod".">"."<img border="."0".""."><img border="."0"." alt="."deletar"." src="."../View/icons/deletar.svg"." width="."30"." height="."30"."></a>".
                                 "</div>";
                                 echo "</tr>";
                             }
